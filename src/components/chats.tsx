a6028727ef9c6a493430eaba3bd99adae270aedf
lojith vinsuka <lojithvinsuka1998@gmail.com>
2023-05-20 22:37:46 +0530

    init

---
+++ src/components/chats.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+type Props = {};
+
+const Chats = (props: Props) => {
+  const chats = ["hello", "hi", "How are you", "I'm fine"];
+  
+  return (
+    <div className="w-full">
+      {chats.map((m, i) => {
+        return i % 2 != 0 ? (
+          <div className="flex w-full justify-start">
+            <div className="bg-[#146C94] p-2">{m}</div>
+          </div>
+        ) : (
+          <div className="flex w-full justify-end">
+            <div className="bg-[#19A7CE] p-2">{m}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Chats;
