2ffde9f7e9f3b2b67372c2f9eab77064d9f2a7c4
lojith vinsuka <lojithvinsuka1998@gmail.com>
2023-05-28 17:24:33 +0530

    fix ui issue

--- src/components/chats.tsx
+++ src/components/chats.tsx
@@ -8,22 +8,31 @@ type Props = {};
 const Chats = (props: Props) => {
   const chats = ["hello", "hi", "How are you", "I'm fine"];
 
-  const msgList = useMsgList()
+  const msgList = useMsgList();
 
   useEffect(() => {
-    setMsgList(["hello", "hi", "How are you", "I'm fine"]);
+    // setMsgList([
+    //   "What is virtual reality (VR)?",
+    //   "Virtual reality (VR) is a computer-generated simulation of a three-dimensional environment that users can interact with and explore. It typically involves the use of specialized headsets and controllers to create an immersive and realistic experience.",
+    //   "What is machine learning?",
+    //   "Machine learning is a subset of artificial intelligence (AI) that focuses on developing algorithms and models that enable computers to learn from and make predictions or decisions based on data without being explicitly programmed. It involves training models on sample data and using them to analyze and interpret new data.",
+    //   "What is a firewall?",
+    //   "A firewall is a network security device that monitors and controls incoming and outgoing network traffic based on predetermined security rules. It acts as a barrier between internal and external networks, protecting against unauthorized access and potential threats.",
+    //   "What is a database?",
+    //   "A database is an organized collection of structured data stored and accessed electronically. It allows users to store, retrieve, and manipulate data efficiently.A database is an organized collection of structured data stored and accessed electronically. It allows users to store, retrieve, and manipulate data efficiently.",
+    // ]);
   }, []);
-  
+
   return (
     <div className="w-full">
       {msgList.map((m, i) => {
         return i % 2 != 0 ? (
           <div key={i} className="flex w-full justify-start m-1">
-            <div className="bg-[#146C94] p-2">{m}</div>
+            <div className="bg-[#146C94] p-2 w-4/5">{m}</div>
           </div>
         ) : (
           <div key={i} className="flex w-full justify-end m-1">
-            <div className="bg-[#19A7CE] p-2">{m}</div>
+            <div className="bg-[#19A7CE] p-2 w-4/5">{m}</div>
           </div>
         );
       })}
